03b6a0738ebdb8ca9a1b689568cdc125d72965b5
Kallil Belmonte <kallil.belmonte@gmail.com>
2024-05-04 18:19:04 -0300

    Replaced npm with bun.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To check out docs, visit [vitepress.vuejs.org](https://vitepress.vuejs.org/).
 
 | Command                  | Description                                                                    |
 | ------------------------ | ------------------------------------------------------------------------------ |
-| `npm install` or `npm i` | Install package.json dependencies, then runs the package.json scripts.install. |
-| `npm run dev`            | Runs the app in the development mode                                           |
-| `npm run serve`          | Converts the current working directory into a virtual directory                |
-| `npm run build`          | Builds the app for production to the `build` folder                            |
+| `bun install` or `bun i` | Install package.json dependencies, then runs the package.json scripts.install. |
+| `bun run dev`            | Runs the app in the development mode                                           |
+| `bun run serve`          | Converts the current working directory into a virtual directory                |
+| `bun run build`          | Builds the app for production to the `build` folder                            |
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
   /**
-   * @see AppConfigs { @link https://vitepress.vuejs.org/config/app-configs }
+   * @see SiteConfig { @link https://vitepress.dev/reference/site-config }
    */
   lang: 'en-US',
   title: 'Demo',
@@ -16,8 +16,7 @@ export default defineConfig({
   ],
 
   /**
-   * @see ThemeConfigs { @link https://vitepress.vuejs.org/config/theme-configs }
-   * @see ThemeIntroduction { @link https://vitepress.vuejs.org/guide/theme-introduction }
+   * @see DefaultThemeConfig { @link https://vitepress.dev/reference/default-theme-config }
    */
   themeConfig: {
     logo: '/images/logo.svg',
@@ -26,13 +25,8 @@ export default defineConfig({
       copyright: 'Copyright © 2022-present',
     },
 
-    localeLinks: {
-      text: 'Lang',
-      items: [{ text: 'Português', link: '/br/' }],
-    },
-
     /**
-     * @see Nav { @link https://vitepress.vuejs.org/guide/theme-nav }
+     * @see Nav { @link https://vitepress.dev/reference/default-theme-config#nav }
      */
     nav: [
       {
@@ -44,29 +38,33 @@ export default defineConfig({
         link: '/config/',
       },
       {
+        text: 'Lang',
+        items: [{ text: 'Português', link: '/br/' }],
+      },
+      {
         text: 'App',
         link: 'https://vitepress.vuejs.org',
       },
     ],
 
     /**
-     * @see Sidebar { @link https://vitepress.vuejs.org/guide/theme-sidebar#sidebar }
+     * @see Sidebar { @link https://vitepress.dev/reference/default-theme-config#sidebar }
      */
     sidebar: {
       '/endpoints/': [
         {
           text: 'General',
-          collapsible: true,
+          collapsed: true,
           items: [
-            { text: 'Introduction', link: 'endpoints/general/' },
+            { text: 'Introduction', link: 'endpoints/general/index' },
             { text: 'Types', link: 'endpoints/general/types' },
           ],
         },
         {
           text: 'Products',
-          collapsible: true,
+          collapsed: true,
           items: [
-            { text: 'Summary', link: 'endpoints/products/' },
+            { text: 'Summary', link: 'endpoints/products/index' },
             { text: 'Types', link: 'endpoints/products/types' },
             { text: 'POST', link: 'endpoints/products/post' },
             { text: 'PUT', link: 'endpoints/products/put' },
@@ -76,9 +74,9 @@ export default defineConfig({
         },
         {
           text: 'Users',
-          collapsible: true,
+          collapsed: true,
           items: [
-            { text: 'Summary', link: 'endpoints/users/' },
+            { text: 'Summary', link: 'endpoints/users/index' },
             { text: 'Types', link: 'endpoints/users/types' },
             { text: 'POST', link: 'endpoints/users/post' },
             { text: 'PUT', link: 'endpoints/users/put' },
@@ -90,15 +88,100 @@ export default defineConfig({
       '/config/': [
         {
           text: 'Config',
-          collapsible: true,
+          collapsed: false,
           items: [
-            { text: 'Getting Started', link: 'config/' },
+            { text: 'Getting Started', link: 'config/index' },
             { text: 'Environments', link: 'config/environments' },
             { text: 'HTTP Verbs', link: 'config/http-verbs' },
             { text: 'HTTP Status', link: 'config/http-status' },
           ],
         },
       ],
+    },
+  },
+
+  /**
+   * @see i18n { @link https://vitepress.dev/guide/i18n }
+   */
+  locales: {
+    br: {
+      label: 'Português',
+      lang: 'pt-BR',
+      link: '/br/',
+      titleTemplate: 'Documentação API',
+      description: 'Uma documentação de API demo.',
+      themeConfig: {
+        footer: {
+          message: 'Lançado sob a licença MIT.',
+          copyright: 'Copyright © 2022-presente',
+        },
+        nav: [
+          {
+            text: 'Endpoints',
+            link: '/br/endpoints/general/',
+          },
+          {
+            text: 'Configuração',
+            link: '/br/config/',
+          },
+          {
+            text: 'Lang',
+            items: [{ text: 'English', link: '/' }],
+          },
+          {
+            text: 'App',
+            link: 'https://vitepress.vuejs.org',
+          },
+        ],
+        sidebar: {
+          '/endpoints/': [
+            {
+              text: 'Geral',
+              collapsed: true,
+              items: [
+                { text: 'Introdução', link: 'endpoints/general/index' },
+                { text: 'Tipos', link: 'endpoints/general/types' },
+              ],
+            },
+            {
+              text: 'Produtos',
+              collapsed: true,
+              items: [
+                { text: 'Sumário', link: 'endpoints/products/index' },
+                { text: 'Tipos', link: 'endpoints/products/types' },
+                { text: 'POST', link: 'endpoints/products/post' },
+                { text: 'PUT', link: 'endpoints/products/put' },
+                { text: 'GET', link: 'endpoints/products/get' },
+                { text: 'DELETE', link: 'endpoints/products/delete' },
+              ],
+            },
+            {
+              text: 'Usuários',
+              collapsed: true,
+              items: [
+                { text: 'Summary', link: 'endpoints/users/index' },
+                { text: 'Tipos', link: 'endpoints/users/types' },
+                { text: 'POST', link: 'endpoints/users/post' },
+                { text: 'PUT', link: 'endpoints/users/put' },
+                { text: 'GET', link: 'endpoints/users/get' },
+                { text: 'DELETE', link: 'endpoints/users/delete' },
+              ],
+            },
+          ],
+          '/config/': [
+            {
+              text: 'Configuração',
+              collapsed: false,
+              items: [
+                { text: 'Começando', link: 'config/index' },
+                { text: 'Ambientes', link: 'config/environments' },
+                { text: 'Verbos HTTP', link: 'config/http-verbs' },
+                { text: 'Status HTTP', link: 'config/http-status' },
+              ],
+            },
+          ],
+        },
+      },
     },
   },
 });
